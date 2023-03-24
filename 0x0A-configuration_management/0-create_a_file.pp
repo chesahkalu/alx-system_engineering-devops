2ef98785a7e0fc7095fde9ea4a43f070de063fbf
chesahkalu <chesahkalu@gmail.com>
2023-03-24 15:53:06 -0700

    first task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file in /tmp with specific requirements
+
+file { 'holberton':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
