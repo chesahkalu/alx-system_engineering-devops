966bace5501ba9b4970f8868cb90f69721c8c2f0
chesahkalu <chesahkalu@gmail.com>
2023-05-11 19:28:00 +0100

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "0x16.api.advanced.project by /u/chesahkalu"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
